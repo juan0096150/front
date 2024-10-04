75d20b797cc8fa9978b33efe4ef414502d1a8917
JnSsanRico <juan0096150@gmail.com>
2024-10-04 18:17:21 -0500

    header esta listo del portafolio esta listo

--- src/controllers/controllers.home.js
+++ src/controllers/controllers.home.js
@@ -1,3 +1,8 @@
 export const home = (req,res)=>{
     res.render("views.home.ejs");
 }
+
+
+export const cluade = (req,res)=>{
+  res.render("views.cluade.ejs");
+}
--- src/routes/routes.home.js
+++ src/routes/routes.home.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import {home} from "../controllers/controllers.home";
+import {cluade, home} from "../controllers/controllers.home";
 
 const rutaHome = Router();
 
 rutaHome.get("/", home)
-
+rutaHome.get("/cluade", cluade)
 export default rutaHome;
