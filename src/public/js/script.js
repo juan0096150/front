b92a66c61e3be2b4993cc9c2fb4df8fa0cdaaf8f
JnSsanRico <juan0096150@gmail.com>
2024-10-08 19:00:10 -0500

    login vista

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('Portafolio cargado correctamente');
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('login cargado correctamente');
 });
+
+document.getElementById('show-register').addEventListener('click', function (e) {
+  e.preventDefault();
+  document.getElementById('login').classList.remove('active');
+  document.getElementById('register').classList.add('active');
+});
+
+document.getElementById('show-login').addEventListener('click', function (e) {
+  e.preventDefault();
+  document.getElementById('register').classList.remove('active');
+  document.getElementById('login').classList.add('active');
+});
+
